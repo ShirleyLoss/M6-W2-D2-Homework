660882c71ec3cfe569a7677ade8306054172d125
Loss Shirley <shirleyloss@LossdeMacBook-Pro.local>
2022-10-12 16:44:08 -0700

    Updated.

--- redux-counter/src/Counter.js
+++ redux-counter/src/Counter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import "./counter.css";
 
 class Counter extends React.Component {
     state = { count: 0 }
@@ -7,36 +8,33 @@ class Counter extends React.Component {
     increment = () => {
         //fill in later
         this.props.dispatch({ type: 'INCREMENT' });
-        this.setState({
-            count: this.state.count + 1
-        });
     }
 
     decrement = () => {
         //fill in later
         this.props.dispatch({ type: 'DECREMENT' });
-        this.setState({
-            count: this.state.count - 1
-        });
     }
+    // reset = () => {
+    //     this.props.dispatch({ type: 'RESET'});
+    // }
 
     render() {
         return (
             <div>
                 <h2>Counter</h2>
-                <div>
+                <div className="display">
                     <span>{this.props.count}</span>
                     <button onClick={this.decrement}>-</button>
                     <button onClick={this.increment}>+</button>
                 </div>
             </div>
-        )
+        );
     }
 }
 
 function mapStateToProps(state) {
     return {
-        count: state.count
+        count: state.count,
     };
 }
 
--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -3,18 +3,14 @@ import { render } from 'react-dom';
 import Counter from './Counter';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
 const initialState = {
-  count: 0
+  count: 0,
 };
 
 function reducer(state = initialState, action) {
   // we'll leave this blank for now
-  // which is the same as `return undefined;`
+  // which is the same as `return undefined;
   switch(action.type) {
     case 'INCREMENT':
       return {
@@ -24,16 +20,13 @@ function reducer(state = initialState, action) {
       return {
         const: state.count - 1
       };
+    // case 'RESET':
+    //   return{
+    //     count: 0,
+    //   };
     default: 
       return state;
   }
-  // if(action.type === "INCREMNENT") {
-  //   return {
-  //     const: state.count + 1
-  //   };
-  // }
-
-  return state;
 }
 
 const store = createStore(reducer);
@@ -45,15 +38,3 @@ const App = () => (
 );
 
 render(<App />, document.getElementById('root'));
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
